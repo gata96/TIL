396ed078dceca12078210c9d6a376cb9451d45e6
gata96 <skswoddl16@gmail.com>
2023-01-10 15:44:23 +0900

    Add today study

---
+++ study/python/Practice/Jan10_Practice.py
@@ -0,0 +1,71 @@
+# 정수 한개 입력받기
+
+# number = int(input())
+# print(number)
+
+# 공백으로 구분된 여러개의 정수 입력 받기
+
+# numbers = list(map(int,input(),split()))
+
+# 공백으로 구분된 여러개의 단어 입력 받기
+
+# string = input().split()
+
+# 공백으로 구분된 2개의 정수 입력 받기
+
+# number(a,b) = list(map(int, input().split()))
+
+# 테스트 케이스 수가 주어지는 입력 받기
+
+# T = int(input()) # 테스트 케이스 수
+
+# for t in range(1, T+1):
+#     print(t)
+#     #입력코드
+#     pass
+
+
+# 문제 1
+
+# number = int(input('# 정수를 출력하세요.'))
+
+# 문제 2
+
+# string = input('단어를 구분해서 출력하세요.').split()
+# print(string)
+
+# 문제 3
+# T = int(input('# 테스트 케이스마다 입력 값을 그대로 출력하세요.'))
+
+# for t in range(1,T+1):
+#     print(t)
+
+#     pass
+
+# 문제 4
+
+# numbers = list(map(int, input('2개 이상의 정수를 출력하세요').split()))
+# print(numbers)
+
+# # 문제 5
+
+# number(a,b) = list(map(int, input('2개의 정수를 출력하세요.')))
+# print(number)
+
+# 문제 6
+
+# string = input('단어를 구분해서 출력하세요').split()
+# print(string)
+
+# 문제 7
+
+# 테스트 케이스마다 입력 값을 그대로 출력하세요.
+
+# T = int(input())
+
+# for t in range(T+1):
+#     choose = list(map(int, input().split()))
+#     print(choose)
+#     pass
+
+
