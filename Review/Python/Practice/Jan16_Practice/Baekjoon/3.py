a35c77724c579a93a5672bc43aa38c0f2194f6e5
gata96 <skswoddl16@gmail.com>
2023-01-16 20:54:57 +0900

    Add

---
+++ Review/Python/Practice/Jan16_Practice/Baekjoon/3.py
@@ -0,0 +1,6 @@
+
+T = int(input())
+
+for t in range(1, T+1):
+    a, b =list(map(int, input().split()))
+    print(a+b)
---
+++ Review/Python/Practice/Jan16_Practice/Baekjoon/6.py
@@ -0,0 +1,7 @@
+T = int(input())
+
+for t in range(1, T+1):
+
+    A, B = list(map(int, input().split()))
+    
+    print(f'Case #{t}: {A} + {B} = {A+B}')
