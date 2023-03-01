ffebbdc84d993254f7b3ad30a8e2ff38acad655f
Gata <121420427+gata96@users.noreply.github.com>
2023-03-01 23:45:41 +0900

    Modified Mar01

--- Python/Algorithm/Mar01-[백준]유기농배추1012.py
+++ Python/Algorithm/Mar01-[백준]유기농배추1012.py
@@ -1,7 +1,10 @@
 import sys
-
+sys.setrecursionlimit(10**6) # 재귀 함수 한도 늘려주기
+input = sys.stdin.readline
+MAX = 50 + 10
 
 # 0. 입력 및 초기화
+T = int(input()) # test case
 M, N, K = map(int, input().split()) # 가로,세로길이 , 배추 개수
 graph = [[False] * (N+1) for _ in range(M+1)]
 visited = [False] * (N+1)
