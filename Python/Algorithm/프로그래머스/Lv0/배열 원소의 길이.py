774c206dfc3894c1107c11cda924fe972f16e543
Gata <121420427+gata96@users.noreply.github.com>
2023-03-04 08:33:13 +0900

    Added python

---
+++ Python/Algorithm/프로그래머스/Lv0/배열 원소의 길이.py
@@ -0,0 +1,5 @@
+strlist = ["We", "are", "the", "world!"]
+answer = []
+for i in range(len(strlist)):
+    print(len(strlist[i]))
+    
---
+++ Python/Algorithm/프로그래머스/Lv0/피자 나눠먹기(3).py
@@ -0,0 +1,20 @@
+'''
+문제 설명
+머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다. 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
+
+입출력 예
+slice	n	result
+7	    10	2
+4	    12	3
+'''
+# 방1
+def solution(slice, n):
+    mok = n // slice
+    if n % slice != 0:
+        mok = mok + 1
+        return mok
+    else:
+        return mok
+# 방2
+def solution(slice, n):   
+     return ((n - 1) // slice) + 1
