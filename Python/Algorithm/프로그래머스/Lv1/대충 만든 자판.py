406a219ea676fc293fc7da8310909f0b8efd60dd
gata96 <skswoddl16@gmail.com>
2023-05-03 15:49:00 +0900

    deleted django 02

--- Python/Algorithm/프로그래머스/Lv1/대충 만든 자판.py
+++
@@ -1,8 +0,0 @@
-keymap = ["ABACD", "BCEFD"]
-targets = ["ABCD","AABB"]
-result = [9,4]
-
-for i in range(len(keymap[i])):
-    for j in range(len(keymap)[j]):
-        lst = list(keymap[i][j])
-    print(lst)
--- django/02_django_design_pattern/articles/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-# app 폴더 안에 view.py 안에서 특정 기능을 수행하는 함수 def를 만든다.
-
-def index(request): 
-    # index는 보통 메인페이지를 의미한다.
-    return render(request, 'index.html')
