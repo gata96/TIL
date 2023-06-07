5ee9008c686dbf45c71d67fb1b25729b73b9641a
gata96 <skswoddl16@gmail.com>
2023-06-07 23:12:12 +0900

    Modified

--- note.py
+++ note.py
@@ -1,8 +1,12 @@
-str1 = "aaaaa"
-str2 = "bbbbb"
-
-
-for i in range(len(str1)):
-    result= ''
-    result += str1[i]+str2[i]
-print(result)
+X = "100"
+Y = "123450"
+answer = []
+for i in set(X)&set(Y):# 교집합
+    answer.append(i)
+    answer = sorted(answer, reverse=True)
+if len(answer) == 0:
+    print('-1') 
+if answer[0] == '0':
+    print('0')
+    
+print(''.join(answer))
